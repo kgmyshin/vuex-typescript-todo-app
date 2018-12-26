be6a69f55472d887e4aff577f538285d167f821a
kgmyshin <kgmyshin82@gmail.com>
2018-12-26 14:29:23 +0900

    Todoモデル作成

---
+++ src/models/Todo.ts
@@ -0,0 +1,14 @@
+export class Todo {
+  public id: string
+  public body: string
+  public hasDone: boolean
+  constructor(
+    id: string,
+    body: string,
+    hasDone: boolean = false
+  ) {
+    this.id = id
+    this.body = body
+    this.hasDone = hasDone
+  }
+}
